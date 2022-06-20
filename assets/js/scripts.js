9352c0c7878ed6cc1587b2ebca06a99372f53793
Angel Mavare <angelmavare1@gmail.com>
2022-06-20 11:38:45 -0400

    array basics

---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+/* Arrays */
+console.log("--------- ARRAYS----------")
+var races = ["Orc", "Elf", "Human", "Dwarf", "Faun", "Centaur", "Minotaur"];
+console.log("Print array: "+ races);
+console.log("Print quantity of items in the array: "+ races.length);
+console.log("Get third item in the array: "+ races[2]);
