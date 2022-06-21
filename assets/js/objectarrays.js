f0b7a6cee276ad42868745c8fee591d846b2ddf6
Angel Mavare <angelmavare1@gmail.com>
2022-06-21 12:37:11 -0400

    Update objectarrays.js

--- assets/js/objectarrays.js
+++ assets/js/objectarrays.js
@@ -1,12 +1,12 @@
 /* Object Arrays */
 
 var characters = [
-    { age: "30", sex: "Man", class: "Spartan", weapon: "Spear", race: "Orc" },
-    { age: "40", sex: "Man", class: "Priest", weapon: "Staff", race: "Faun" },
-    { age: "25", sex: "Woman", class: "Ninja", weapon: "Kusarigama", race: "Human" },
-    { age: "45", sex: "Man", class: "Samurai", weapon: "Katana", race: "Human" },
-    { age: "34", sex: "Woman", class: "Paladin", weapon: "Claymore", race: "Elf" },
-    { age: "19", sex: "Woman", class: "Rogue", weapon: "Daggers", race: "Elf" },
+    { age: 30, sex: "Man", class: "Spartan", weapon: "Spear", race: "Orc" },
+    { age: 40, sex: "Man", class: "Priest", weapon: "Staff", race: "Faun" },
+    { age: 25, sex: "Woman", class: "Ninja", weapon: "Kusarigama", race: "Human" },
+    { age: 45, sex: "Man", class: "Samurai", weapon: "Katana", race: "Human" },
+    { age: 34, sex: "Woman", class: "Paladin", weapon: "Claymore", race: "Elf" },
+    { age: 19, sex: "Woman", class: "Rogue", weapon: "Daggers", race: "Elf" }
 ]
 
 /* Array filter:  */
@@ -24,3 +24,24 @@ var characterClasses = characters.map(function(character){
 })
 console.log(characterClasses);
 
+/* Find */
+/* Return the first element that matches the function */
+var findCharacter = characters.find(function(character){
+    return character.age == 25;
+})
+console.log(findCharacter);
+
+/* Some */
+/* Return true or false if find at least one element that matches the function */
+var someCharacter = characters.some(function(character){
+    return character.age >= 40;
+})
+console.log(someCharacter);
+
+/* ForEach */
+/* Make a loop for all elements with a function that process the data */
+var forEachCharacter = characters.forEach(function(character){
+    return console.log(character.class);
+})
+
+//console.log(forEachCharacter);
