61cbd92228a9d2994ccd6871421b83187bedbf24
Angel Mavare <angelmavare1@gmail.com>
2022-06-23 14:47:39 -0400

    Update es6.js

--- assets/js/es6.js
+++ assets/js/es6.js
@@ -34,3 +34,35 @@ let arrayAnimals2 = ['Dog', 'Owl', 'Tiger', 'Wale'];
 let arrayAllAnimals = [...arrayAnimals1, ...arrayAnimals2];
 console.log(arrayAllAnimals);
 
+//Object building
+let colorEx = 'Red';
+let ageEx = '30';
+let sexEx = 'woman';
+
+let objEx = { colorEx, ageEx, sexEx};
+console.log(objEx);
+
+//Arrow Functions
+
+let people = [
+    {name: 'Julian', sex: 'Male', age:'66'},
+    {name: 'Maria', sex: 'Female', age:'35'},
+    {name: 'Camila', sex: 'Female', age:'22'}
+]
+//ES5 example: return all names
+console.log('----------ES 5----------');
+let peopleNames = people.map(function(person){
+    console.log(person.name);
+});
+
+//ES6 example
+console.log('----------ES 6----------');
+let peopleNames3 = people.map(person => console.log(person.name));
+
+//ES6 example 2
+const height = 500;
+const width = 400;
+let arrowFunction1 = (h,w) => console.log(h*w);
+arrowFunction1(height, width);
+
+//PROMISES...
