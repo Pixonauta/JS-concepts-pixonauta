e870748c102e7941431d7f0699a57b2d1eba05f7
Angel Mavare <angelmavare1@gmail.com>
2022-06-24 12:15:57 -0400

    Create es8.js

---
+++ assets/js/es8.js
@@ -0,0 +1,17 @@
+/************************************
+ *          ECMASript 8
+ ***********************************/
+
+const arrayExampleES8 = {
+    color: 'blue',
+    age: '29',
+    sex: 'Male'
+}
+
+//OBJECT Entries
+const entries = Object.entries(arrayExampleES8)
+console.log(entries);
+
+//OBJECT Values
+const values = Object.values(arrayExampleES8)
+console.log(values);
