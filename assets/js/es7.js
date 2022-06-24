4ac19f7c2f20836ed0062ffeb42949ec283cb964
Angel Mavare <angelmavare1@gmail.com>
2022-06-24 11:14:56 -0400

    Update es7.js

--- assets/js/es7.js
+++ assets/js/es7.js
@@ -2,3 +2,17 @@
  *          ECMASript 7
  ***********************************/
 
+// INCLUDES
+//Includes checks if a value exists in an array
+let arrayES7 = [1, 2, 3, 4, 5, 6, 7];
+if(arrayES7.includes(8)){
+    console.log("The Value is here!");
+}else{
+    console.log("The value isn't here");
+}
+
+//EXPONENT
+let base = 2;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
